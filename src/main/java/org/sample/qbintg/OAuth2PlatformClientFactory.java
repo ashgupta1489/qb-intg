8ed44572906297f03b649c8a5d6a094a5a2775cd
Ashish Gupta <ashgupta1489@users.noreply.github.com>
2017-11-14 16:26:06 -0500

    commit

--- src/main/java/org/sample/qbintg/OAuth2PlatformClientFactory.java
+++ src/main/java/org/sample/qbintg/OAuth2PlatformClientFactory.java
@@ -12,7 +12,7 @@ import com.intuit.oauth2.config.Environment;
 import com.intuit.oauth2.config.OAuth2Config;
 
 @Service
-@PropertySource(value="classpath:/application.properties", ignoreResourceNotFound=true)
+@PropertySource(value="classpath:/application.properties", ignoreResourceNotFound=false)
 public class OAuth2PlatformClientFactory {
 	
 	private static final Logger logger = Logger.getLogger(OAuth2PlatformClientFactory.class);
@@ -29,6 +29,15 @@ public class OAuth2PlatformClientFactory {
 		oauth2Config = new OAuth2Config.OAuth2ConfigBuilder(env.getProperty("OAuth2AppClientId"), env.getProperty("OAuth2AppClientSecret")) //set client id, secret
 				.callDiscoveryAPI(Environment.SANDBOX) // call discovery API to populate urls
 				.buildConfig();
+		
+		logger.info("clientId"+oauth2Config.getClientId());
+		logger.info("clientSecret"+oauth2Config.getClientSecret());
+		logger.info("intuitIdTokenIssuer"+oauth2Config.getIntuitIdTokenIssuer());
+		logger.info("intuitAuthorizationEndpoint"+oauth2Config.getIntuitAuthorizationEndpoint());
+		logger.info("intuitBearerTokenEndpoint"+oauth2Config.getIntuitBearerTokenEndpoint());
+		logger.info("intuitRevokeTokenEndpoint"+oauth2Config.getIntuitRevokeTokenEndpoint());
+		logger.info("intuitJwksURI"+oauth2Config.getIntuitJwksURI());
+		logger.info("userProfileEndpoint"+oauth2Config.getUserProfileEndpoint());
 		client  = new OAuth2PlatformClient(oauth2Config);
 	}
 	
